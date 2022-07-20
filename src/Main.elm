0af08ec5fc22df42f120021114f1f8215b56537a
Austin Sharman <asharman22@gmail.com>
2022-07-20 08:36:11 -0400

    Set up Elm + Vite

---
+++ src/Main.elm
@@ -0,0 +1,30 @@
+module Main exposing (main)
+
+import Browser
+import HelloWorld exposing (helloWorld)
+import Html exposing (Html, div, img)
+import Html.Attributes exposing (src, style)
+import Msg exposing (Msg(..))
+
+
+main : Program () Int Msg
+main =
+    Browser.sandbox { init = 0, update = update, view = view }
+
+
+update : Msg -> number -> number
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+view : Int -> Html Msg
+view model =
+    div []
+        [ img [ src "/logo.png", style "width" "300px" ] []
+        , helloWorld model
+        ]
