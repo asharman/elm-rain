168026a4a6ede408526d10bfaca06d96daac9712
Austin Sharman <asharman22@gmail.com>
2022-07-24 12:13:22 -0400

    add fullscreen support

--- src/Main.elm
+++ src/Main.elm
@@ -2,34 +2,20 @@ module Main exposing (main)
 
 import Arrow
 import Browser
-import Browser.Events exposing (onAnimationFrameDelta)
+import Browser.Dom exposing (Viewport, getViewport)
+import Browser.Events exposing (onAnimationFrameDelta, onResize)
 import Canvas
 import Canvas.Settings as Canvas
 import Canvas.Settings.Advanced as Canvas
 import Canvas.Settings.Line as Canvas
 import Color
 import Html exposing (Html, div)
-import Html.Attributes exposing (style)
+import Html.Attributes exposing (style, width)
+import Task
 
 
 
 -- CONSTANTS
-
-
-width =
-    1000
-
-
-height =
-    800
-
-
-centerX =
-    width / 2
-
-
-centerY =
-    height / 2
 
 
 origin : Canvas.Point
@@ -38,47 +24,79 @@ origin =
 
 
 type alias Model =
-    { position : Canvas.Point }
+    { width : Float, height : Float, position : Canvas.Point }
 
 
 initialModel =
-    { position = ( centerX, centerY ) }
+    { width = 400, height = 400, position = ( 200, 200 ) }
 
 
 type Msg
     = Frame Float
+    | GetViewPort Viewport
+    | BrowserResized Int Int
 
 
 main : Program () Model Msg
 main =
     Browser.element
-        { init = \() -> ( initialModel, Cmd.none )
+        { init = \() -> ( initialModel, Task.perform GetViewPort getViewport )
         , view = view
         , update =
             \msg model ->
                 case msg of
                     Frame _ ->
                         ( model, Cmd.none )
-        , subscriptions = \model -> onAnimationFrameDelta Frame
+
+                    GetViewPort data ->
+                        ( { model
+                            | width = data.viewport.width
+                            , height = data.viewport.height
+                            , position = ( data.viewport.width / 2, data.viewport.height / 2 )
+                          }
+                        , Cmd.none
+                        )
+
+                    BrowserResized w h ->
+                        let
+                            width =
+                                toFloat w
+
+                            height =
+                                toFloat h
+                        in
+                        ( { model
+                            | width = width
+                            , height = height
+                            , position = ( width / 2, height / 2 )
+                          }
+                        , Cmd.none
+                        )
+        , subscriptions =
+            \_ ->
+                Sub.batch
+                    [ onAnimationFrameDelta Frame
+                    , onResize BrowserResized
+                    ]
         }
 
 
 view : Model -> Html Msg
-view { position } =
+view { width, height, position } =
     div
         [ style "display" "flex"
         , style "justify-content" "center"
         , style "align-items" "center"
         ]
         [ Canvas.toHtml
-            ( width, height )
+            ( round width, round height )
             []
-            [ clearScreen
+            [ clearScreen width height
             , Arrow.render { from = origin, to = position, color = Color.rgb 0.3 0.3 1.0 }
             ]
         ]
 
 
-clearScreen : Canvas.Renderable
-clearScreen =
+clearScreen : Float -> Float -> Canvas.Renderable
+clearScreen width height =
     Canvas.shapes [ Canvas.fill Color.darkCharcoal ] [ Canvas.rect ( 0, 0 ) width height ]
